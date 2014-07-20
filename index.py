c261537be7cb0ea01d5498562fdb233c8fa8deca
niyuna <niyuna@gmail.com>
2014-07-20 20:57:21 +0800

    django init

--- index.py
+++ index.py
@@ -1,11 +1,15 @@
 #-*- coding:utf-8 -*-
 
-def app(environ, start_response):
-    status = '200 OK'
-    headers = [('Content-type', 'text/html')]
-    start_response(status, headers)
-    body=["Welcome to Baidu Cloud!\n"]
-    return body
+# def app(environ, start_response):
+#     status = '200 OK'
+#     headers = [('Content-type', 'text/html')]
+#     start_response(status, headers)
+#     body=["Welcome to Baidu Cloud!\n"]
+#     return body
 
-from bae.core.wsgi import WSGIApplication
-application = WSGIApplication(app)
+# from bae.core.wsgi import WSGIApplication
+# application = WSGIApplication(app)
+
+
+import django
+return "django version: " + django.get_version()
