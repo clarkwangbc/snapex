04d0e3c0ad42d91850c1e75e46b6ef3ef1ed0a1d
niyuna <niyuna@gmail.com>
2014-08-03 09:53:23 +0800

    update global django settings

--- snapex/settings.py
+++ snapex/settings.py
@@ -102,10 +102,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'polls.urls'
+ROOT_URLCONF = 'snapex.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'polls.wsgi.application'
+WSGI_APPLICATION = 'snapex.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
