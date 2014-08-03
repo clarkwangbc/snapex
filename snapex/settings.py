4512b2de5da7cd9f362b533c07c6041536c49777
niyuna <niyuna@gmail.com>
2014-08-03 16:04:47 +0800

    test

--- snapex/settings.py
+++ snapex/settings.py
@@ -116,9 +116,9 @@ TEMPLATE_DIRS = (
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    # 'django.contrib.sessions',
-    # 'django.contrib.sites',
-    # 'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
