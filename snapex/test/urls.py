ad4689f6517ca13aac27db73a0658974c783f884
niyuna <niyuna@gmail.com>
2014-08-03 15:19:33 +0800

    test runcmd

--- snapex/test/urls.py
+++ snapex/test/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^dbtest$', views.index, name='index')
+    url(r'^dbtest$', views.dbtest, name='dbtest'),
+    url(r'^runcmd$', views.runcmd, name='runcmd')
 ]
--- snapex/test/views.py
+++ snapex/test/views.py
@@ -1,28 +1,37 @@
 from django.http import HttpResponse
 
 def testcreateTable():
-   dbname = "beXsKRIOGfKKTwkkcTkh"
-   import MySQLdb
-   mydb = MySQLdb.connect(
-      host   = 'sqld.duapp.com',
-      port   = 4050,
-      user   = '4vvtke0DV3yR9bIYcGyDvKBC',
-      passwd = '1B65i354OUTyyyVxMhI9IlgBxFztCp84',
-      db = 'beXsKRIOGfKKTwkkcTkh')
- 
-   cursor = mydb.cursor()
- 
-   cmd = '''create table employee (
-         id int(4) auto_increment,
-         name char(20) not null,
-         age int(2),
-         sex char(8) default 'man',
-         primary key (id))'''
- 
-   cursor.execute(cmd)
- 
-   mydb.close()
+    dbname = "beXsKRIOGfKKTwkkcTkh"
+    import MySQLdb
+    mydb = MySQLdb.connect(
+        host   = 'sqld.duapp.com',
+        port   = 4050,
+        user   = '4vvtke0DV3yR9bIYcGyDvKBC',
+        passwd = '1B65i354OUTyyyVxMhI9IlgBxFztCp84',
+        db = 'beXsKRIOGfKKTwkkcTkh')
 
-def index(request):
+    cursor = mydb.cursor()
+
+    cmd = '''create table employee (
+             id int(4) auto_increment,
+             name char(20) not null,
+             age int(2),
+             sex char(8) default 'man',
+             primary key (id))'''
+
+    cursor.execute(cmd)
+
+    mydb.close()
+
+def excutecmd():
+    from django.core.management import call_command
+    call_command('validate')
+
+def dbtest(request):
     testcreateTable()
+    return HttpResponse('complete')
+
+def runcmd(request):
+    excutedcmd()
+    print 'cmd runed well'
     return HttpResponse('complete')
