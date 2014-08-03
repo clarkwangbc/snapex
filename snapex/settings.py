5fa6099cf2b2bf87846f40f704c1f23bf931bdb9
niyuna <niyuna@gmail.com>
2014-08-03 15:51:01 +0800

    test

--- snapex/settings.py
+++ snapex/settings.py
@@ -154,7 +154,7 @@ LOGGING = {
         'logfile': {
             'level':'DEBUG',
             'class':'logging.handlers.RotatingFileHandler',
-            'filename': '/home/bae/log/logfile',
+            'filename': '/home/bae/log/debug.log.temp',
             'maxBytes': 50000,
             'backupCount': 2,
             'formatter': 'standard',
