ce46aa296a5a64de876c6b862a9f0a29c830518b
niyuna <niyuna@gmail.com>
2014-08-11 10:38:20 +0800

    add related_name

--- snapex/polls/models.py
+++ snapex/polls/models.py
@@ -11,18 +11,18 @@ class User(models.Model):
 		return u'%s'%(secret) if secret is not None else u'None'
 
 class Project(models.Model):
-	owner = models.ForeignKey(User)
+	owner = models.ForeignKey(User, related_name='owner')
 	subject = models.CharField(default='', max_length=100)
-	researchers = models.ManyToManyField(User)
-	testees = models.ManyToManyField(User)
+	researchers = models.ManyToManyField(User, related_name='researchers')
+	testees = models.ManyToManyField(User, related_name='testees')
 	date_created = models.DateTimeField(auto_now=True)
 
 	def __unicode__(self):
 		return u'%s by %s'%(subject, owner)
 
 class Survey(models.Model):
-	creater = models.ForeignKey(User)
-	project = models.ForeignKey(Project)
+	creater = models.ForeignKey(User, related_name='creater')
+	project = models.ForeignKey(Project, related_name='project')
 	content = models.TextField(default='')
 	date_created = models.DateTimeField(auto_now=True)
 
@@ -30,18 +30,18 @@ class Survey(models.Model):
 		return u'%s @ %s'%(creater, date_created)
 
 class Record(models.Model):
-	testee = models.ForeignKey(User)
+	testee = models.ForeignKey(User, related_name='testee')
 	date_created = models.DateTimeField(auto_now=True)
-	survey = models.ForeignKey(Survey)
+	survey = models.ForeignKey(Survey, related_name='survey')
 	reply = models.TextField(default='')
 	
 	def __unicode__(self):
 		return u'%s @ %s'%(testee, date_created)
 
 class Plan(models.Model):
-	survey = models.ForeignKey(Survey)
-	owner = models.ForeignKey(User)
-	testee = models.ForeignKey(User)
+	survey = models.ForeignKey(Survey, related_name='survey')
+	owner = models.ForeignKey(User, related_name='owner')
+	testee = models.ForeignKey(User, related_name='testee')
 	is_sent = models.BooleanField(default=False)
 	is_done = models.BooleanField(default=False)
 	schedule = models.TextField(default='')
