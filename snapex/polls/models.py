48c124f462eb3737be3928e02f65def1f366003f
niyuna <niyuna@gmail.com>
2014-08-11 10:44:53 +0800

    modify related_names

--- snapex/polls/models.py
+++ snapex/polls/models.py
@@ -11,18 +11,18 @@ class User(models.Model):
 		return u'%s'%(secret) if secret is not None else u'None'
 
 class Project(models.Model):
-	owner = models.ForeignKey(User, related_name='owner')
+	owner = models.ForeignKey(User, related_name='project_owner')
 	subject = models.CharField(default='', max_length=100)
-	researchers = models.ManyToManyField(User, related_name='researchers')
-	testees = models.ManyToManyField(User, related_name='testees')
+	researchers = models.ManyToManyField(User, related_name='project_researchers')
+	testees = models.ManyToManyField(User, related_name='project_testees')
 	date_created = models.DateTimeField(auto_now=True)
 
 	def __unicode__(self):
 		return u'%s by %s'%(subject, owner)
 
 class Survey(models.Model):
-	creater = models.ForeignKey(User, related_name='creater')
-	project = models.ForeignKey(Project, related_name='project')
+	creater = models.ForeignKey(User, related_name='survey_creater')
+	project = models.ForeignKey(Project, related_name='survey_project')
 	content = models.TextField(default='')
 	date_created = models.DateTimeField(auto_now=True)
 
@@ -30,18 +30,18 @@ class Survey(models.Model):
 		return u'%s @ %s'%(creater, date_created)
 
 class Record(models.Model):
-	testee = models.ForeignKey(User, related_name='testee')
+	testee = models.ForeignKey(User, related_name='record_testee')
 	date_created = models.DateTimeField(auto_now=True)
-	survey = models.ForeignKey(Survey, related_name='survey')
+	survey = models.ForeignKey(Survey, related_name='record_survey')
 	reply = models.TextField(default='')
 	
 	def __unicode__(self):
 		return u'%s @ %s'%(testee, date_created)
 
 class Plan(models.Model):
-	survey = models.ForeignKey(Survey, related_name='survey')
-	owner = models.ForeignKey(User, related_name='owner')
-	testee = models.ForeignKey(User, related_name='testee')
+	survey = models.ForeignKey(Survey, related_name='plan_survey')
+	owner = models.ForeignKey(User, related_name='plan_owner')
+	testee = models.ForeignKey(User, related_name='plan_testee')
 	is_sent = models.BooleanField(default=False)
 	is_done = models.BooleanField(default=False)
 	schedule = models.TextField(default='')
