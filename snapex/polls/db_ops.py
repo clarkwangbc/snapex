bd04407e9cbcc024fdf2c9f3c7125e032057cd46
niyuna <niyuna@gmail.com>
2014-08-11 11:46:27 +0800

    test

---
+++ snapex/polls/db_ops.py
@@ -0,0 +1,26 @@
+from models import *
+
+# user
+def create_user(secret, is_admin=False, is_researcher=False, is_activated=False):
+	user = User(secret=secret, 
+					is_admin=is_admin, 
+					is_researcher=is_researcher,
+					is_activated=is_activated)
+	user.save()
+	
+def create_admin():
+	create_user(secret=u'19770707',
+					is_admin=True,
+					is_researcher=True,
+					is_activated=True)
+
+def create_researcher():
+	import uuid
+	create_user(secret=unicode(uuid.uuid1()),
+					is_researcher=True)
+
+def create_testees(number=10):
+	import uuid
+	for i in range(number):
+		create_user(secret=unicode(uuid.uuid1()))
+
