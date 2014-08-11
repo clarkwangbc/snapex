331f987b18fce9679414c5d15e355836fc11c435
niyuna <niyuna@gmail.com>
2014-08-11 15:30:34 +0800

    add create_project

--- snapex/polls/db_ops.py
+++ snapex/polls/db_ops.py
@@ -16,14 +16,88 @@ def create_admin():
 
 def create_researcher():
 	import uuid
-	create_user(secret=unicode(uuid.uuid1()),
+	secret = unicode(uuid.uuid1()
+	create_user(secret=secret),
 					is_researcher=True)
+	return secret
 
 def create_testees(number=10):
 	import uuid
-	for i in range(number):
-		create_user(secret=unicode(uuid.uuid1()))
+	if number<0:
+		return
+	secrets = [unicode(uuid.uuid1() for i in range(number)]
+	for s in secrets:
+		create_user(secret=s)
+	return secrets
 
 # project
-def create_project(owner, subject, 	researchers, testees):
+def create_project(owner='', subject='', researchers=[], testees=[]):
+	'''
+	owner: string, secret of a owner user
+	subject: string, subject of the project
+	researchers: iterable of strings, secret of the researcher users
+	testees: iterable of stirngs, secret of the testee users
+	return: (0,project_id) if success; (1,msg) if failed
+	'''
+	# validate owner
+	if owner=='':
+		owner = create_researcher()
+	else:
+		if not User.objects.filter(secret=unicode(owner)).exists():
+			return 1, 'owner user not exist'
+
+	# validate researchers
+	for r in researchers:
+		if not User.objects.filter(secret=unicode(r)).exists():
+			return 1, 'some researchers not exist'
+
+	# validate testees
+	for t in testees:
+		if not User.objects.filter(secret=unicode(t)).exists():
+			return 1, 'some testees not exist'
+
+	owner_user = User.objects.get(secret=owner)
+	project = Project(owner=owner_user, subject=subject)
+	project.save()
+	project.researchers.add(*User.objects.filter(secret__in=researchers))
+	project.testees.add(*User.objects.filter(secret__in=testees))
+	return 0, project.id
+
+
+def add_testees_to_project(owner, subject, testees):
+	'''
+	testee: iterable of strings, secret of the new testees
+	return: 0 if success; (1,msg) if failed
+	'''
+	pass
+
+# survey
+def create_survey(creater, project, content):
+	'''
+	creater: string, secret of a researcher
+	project: string, subject of the project
+	content: string, formatted survey content
+	return: (0,survey_id) if success; (1,msg) if failed
+	'''
+	pass
+
+# record
+def create_record(testee, survey, reply):
+	'''
+	testee: string, secret of a testee
+	survey: int, survey_id
+	reply: string, formatted survey reply to the survey
+	return: (0,record_id) if success; (1,msg) if failed
+	'''
+	pass
+
+# plan
+def create_plan(survey, owner, testee, is_sent=False, is_done=False, schedule):
+	'''
+	survey: int, survey_id
+	owner: string, secret of the owner researcher
+	testee: string, secret of the testee
+	schedule: string, formatted schedule string
+	return: (0,plan_id) if success; (1,msg) if failed
+	'''
 	pass
--- snapex/polls/views.py
+++ snapex/polls/views.py
@@ -5,8 +5,12 @@ def index(request):
 
 def test(request):
 	import db_ops
-	db_ops.create_admin()
-	db_ops.create_researcher()
-	db_ops.create_testees()
+	# db_ops.create_admin()
+	# db_ops.create_researcher()
+	# db_ops.create_testees()
+	i = create_project(owner='19770707', 
+					subject='test', 
+					researchers=['19770707'], 
+					testees=['19770707'])
 
-	return HttpResponse("I'm testing...")
+	return HttpResponse("I'm testing... %s %s"%(i))
