527cb16ff6514b05abb5409707b36dca8258de4c
niyuna <niyuna@gmail.com>
2014-08-16 13:25:32 +0800

    test

--- snapex/urls.py
+++ snapex/urls.py
@@ -7,8 +7,8 @@ admin.autodiscover()
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^signin/', include('singin.urls')),
-    url(r'^polls/', include('polls.urls')),
+	url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^test/', include('test.urls')),
+    url(r'^signin/', include('singin.urls')),
 ]
