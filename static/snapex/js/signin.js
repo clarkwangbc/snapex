9203595bd08047eb61e32f2171a7c2f1b126f548
niyuna <niyuna@gmail.com>
2014-08-16 16:48:23 +0800

    test

--- static/snapex/js/signin.js
+++ static/snapex/js/signin.js
@@ -1,9 +1,13 @@
 login = function(){
-	$.ajax("/api/v0/signin", {
-		test: "test",
-		secret: $("#input_secret").value,
-	}).done(function(data){
-		console.log(data.status);
-		console.log(data.secret);
-	});
+	input_secret = $("#input_secret").value;
+	if (input_secret) {
+		$.ajax("/api/v0/signin", {
+			test: "test",
+			secret: input_secret,
+		}).done(function(data){
+			console.log(data.status);
+			console.log(data.secret);
+		});
+	}
+	return 0;
 }
