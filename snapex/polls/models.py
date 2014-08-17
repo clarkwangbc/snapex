7d73b006539110b4af19fe3649c313cd235447cf
niyuna <niyuna@gmail.com>
2014-08-17 15:28:04 +0800

    revise models

--- snapex/polls/models.py
+++ snapex/polls/models.py
@@ -1,54 +1,104 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class UserProfile(models.Model):
-	user = models.OneToOneField(User, related_name='user_profile')
 	device_id = models.CharField(max_length=100)
 	is_admin = models.BooleanField(default=False)
 	is_researcher = models.BooleanField(default=False)
 	
+	user = models.OneToOneField(User, related_name='user_profile')
+
 	def __unicode__(self):
 		return u'%s'%(secret) if secret is not None else u'None'
 
 	def __str__(self):
 		return "%s's profile" % self.user  
 
+
 class Project(models.Model):
-	owner = models.ForeignKey(User, related_name='project_owner')
+	owner = models.ForeignKey(User, related_name='owner_projects')
+	# researchers = models.ManyToManyField(User, related_name='researchers_projects')
+	testees = models.ManyToManyField(User, 
+					through='ProjectTesteeMembership',
+					related_name='testees_projects')
+	
+	name = models.CharField(max_length=50)
 	subject = models.CharField(default='', max_length=100)
-	researchers = models.ManyToManyField(User, related_name='project_researchers')
-	testees = models.ManyToManyField(User, related_name='project_testees')
 	date_created = models.DateTimeField(auto_now=True)
 
 	def __unicode__(self):
 		return u'%s by %s'%(subject, owner)
 
-class Survey(models.Model):
-	creater = models.ForeignKey(User, related_name='survey_creater')
-	project = models.ForeignKey(Project, related_name='survey_project')
-	content = models.TextField(default='')
-	date_created = models.DateTimeField(auto_now=True)
 
+class ProjectTesteeMembership(models.Model):
+	project = models.ForeignKey(Project, related_name='project_testee_memberships')
+	testee = models.ForeignKey(User, related_name='testee_project_memberships')
+
+	alias = models.CharField(max_length=50) # testee alias in each project
+
+
+class QuestionEntry(models.Model):
+	qtype = models.CharField(max_length=10)
+	content = models.TextField() # json
+
+
+class Survey(models.Model):
+	# creater = models.ForeignKey(User, related_name='creater_surveys')
+	project = models.ForeignKey(Project, related_name='project_surveys')
+	questions = models.ManyToManyField(QuestionEntry, 
+					through='SurveyMembership', 
+					related_name='questions_surveys')
+
+	name = models.CharField(max_length=50)
+	date_created = models.DateTimeField(auto_now=True)
+	
 	def __unicode__(self):
 		return u'%s @ %s'%(creater, date_created)
 
-class Record(models.Model):
-	testee = models.ForeignKey(User, related_name='record_testee')
+
+class SurveyMembership(models.Model):
+	qentry = models.ForeignKey(QuestionEntry, related_name='question_memberships')
+	survey = models.ForeignKey(Survey, related_name='survey_memberships')
+
+	entry_order = models.IntegerField(default=0) # used to store qentry order in each survey
+
+
+class Schedule(models.Model):
+	owner = models.ForeignKey(User, related_name='owner_schedules')
+
+	name = models.CharField(max_length=50)
+	content = models.TextField(default='') # json
 	date_created = models.DateTimeField(auto_now=True)
-	survey = models.ForeignKey(Survey, related_name='record_survey')
-	reply = models.TextField(default='')
-	
-	def __unicode__(self):
-		return u'%s @ %s'%(testee, date_created)
+
 
 class Plan(models.Model):
-	survey = models.ForeignKey(Survey, related_name='plan_survey')
-	owner = models.ForeignKey(User, related_name='plan_owner')
-	testee = models.ForeignKey(User, related_name='plan_testee')
+	survey = models.ForeignKey(Survey, related_name='survey_plans')
+	owner = models.ForeignKey(User, related_name='owner_plans')
+	testee = models.ForeignKey(User, related_name='testee_plans')
+	project = models.ForeignKey(Project, related_name='project_plans')
+	schedule = models.ForeignKey(Schedule, related_name='schedule_plans')
+
 	is_sent = models.BooleanField(default=False)
 	is_done = models.BooleanField(default=False)
-	schedule = models.TextField(default='')
 	date_created = models.DateTimeField(auto_now=True)
 	
 	def __unicode__(self):
 		return u'%s to %s @ %s'%(owner, testee, date_created)
+
+
+class Record(models.Model):
+	testee = models.ForeignKey(User, related_name='testee_records')
+	plan = models.ForeignKey(Plan, related_name='plan_records')
+	
+	date_created = models.DateTimeField(auto_now=True)
+
+	def __unicode__(self):
+		return u'%s @ %s'%(testee, date_created)
+
+
+class AnswerEntry(models.Model):
+	qentry = models.ForeignKey(QuestionEntry, related_name='question_aentries')
+	record = models.ForeignKey(Record, related_name='record_aentries')
+	
+	content = models.TextField() # json
