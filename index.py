51a85cbe5071536d6c4645a220799162b0e24ab3
niyuna <niyuna@gmail.com>
2014-08-26 17:23:26 +0800

    test

--- index.py
+++ index.py
@@ -7,9 +7,6 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'snapex.settings'
 path = os.path.dirname(os.path.abspath(__file__)) + '/snapex'
 if path not in sys.path:
     sys.path.insert(1, path)
- 
-from django.core.management import call_command
-call_command('flush')
 
 from django.core.handlers.wsgi import WSGIHandler
 from bae.core.wsgi import WSGIApplication
