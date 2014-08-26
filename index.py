06c3b99599684ba7c7487efed0c8b68ebc78be83
niyuna <niyuna@gmail.com>
2014-08-26 17:17:16 +0800

    test

--- index.py
+++ index.py
@@ -8,6 +8,9 @@ path = os.path.dirname(os.path.abspath(__file__)) + '/snapex'
 if path not in sys.path:
     sys.path.insert(1, path)
  
+from django.core.management import call_command
+call_command('syncdb', interactive=True)
+ 
 from django.core.handlers.wsgi import WSGIHandler
 from bae.core.wsgi import WSGIApplication
  
