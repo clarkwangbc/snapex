2958dadfa4efe4f8212fd792d8d89567a5915e79
niyuna <niyuna@gmail.com>
2014-08-26 16:49:49 +0800

    test

--- snapex/mypage/urls.py
+++ snapex/mypage/urls.py
@@ -4,7 +4,6 @@ import views
 
 urlpatterns = [
 	url(r'^$', views.mypage, name='mypage'),
-	url(r'^user$', views.q_user, name='mypage_user'),
 	url(r'^project$', views.myproject, name='mypage_project'),
 	url(r'^survey$', views.mysurvey, name="mypage_survey"),
 	url(r'^schedule$', views.myschedule, name="mypage_schedule"),
