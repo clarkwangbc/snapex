b9e7d45b3f1b3bea0aa4f7a97221b5b671f28006
Yuming Tao <Yuming@tu059057.ip.tsinghua.edu.cn>
2014-09-16 15:13:00 +0800

    create testees

--- snapex/test/urls.py
+++ snapex/test/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^syncdb$', views.syncdb, name='test_syncdb'),
     url(r'^flush$', views.flush, name='test_flush'),
     url(r'^push_all$', views.push_all, name='test_push_all_plan')
+    url(r'^create_testees', view.create_testees, name='create_testees')
 ]
