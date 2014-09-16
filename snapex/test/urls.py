a89b943e81953bf9eaf1e6aacaf85cb7ebb54188
Yuming Tao <Yuming@tu059057.ip.tsinghua.edu.cn>
2014-09-16 15:20:19 +0800

    fix syntax error

--- snapex/test/urls.py
+++ snapex/test/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^base$', views.base, name='test_base'),
     url(r'^syncdb$', views.syncdb, name='test_syncdb'),
     url(r'^flush$', views.flush, name='test_flush'),
-    url(r'^push_all$', views.push_all, name='test_push_all_plan')
+    url(r'^push_all$', views.push_all, name='test_push_all_plan'),
     url(r'^create_testees$', view.create_testees, name='create_testees')
 ]
