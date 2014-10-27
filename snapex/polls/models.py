5576351892315dd835f755b7ddd8db2199aff227
Yuming Tao <Yuming@tu059120.ip.tsinghua.edu.cn>
2014-10-27 16:28:01 +0800

    model modified version

--- snapex/polls/models.py
+++ snapex/polls/models.py
@@ -17,7 +17,7 @@ class UserProfile(models.Model):
     telephone = models.CharField(max_length=15)
     organization = models.CharField(max_length=100)
     others = models.TextField()
-    remarks = models.CharField(max_length=100)
+    remarks = models.CharField(max_length=100) # 备注信息
     
     def __unicode__(self):
 #        return u'%s'%(secret) if secret is not None else u'None'
@@ -36,25 +36,18 @@ class Project(models.Model):
     code = models.CharField(max_length=4)
     subject = models.CharField(default='', max_length=100)
     date_created = models.DateTimeField(auto_now=True)
-
+    others = models.TextField() # other information or extensions
+    md5 = models.CharField(max_length=128) # support maximum MD5 128
+    
     def __unicode__(self):
         return u'%s by %s'%(subject, owner)
 
 
-class ProjectTesteeMembership(models.Model):
-    project = models.ForeignKey(Project, related_name='project_testee_memberships')
-    testee = models.ForeignKey(User, related_name='testee_project_memberships')
-
-    alias = models.CharField(max_length=50) # testee alias in each project
-
-
-class QuestionEntry(models.Model):
-    qtype = models.CharField(max_length=20)
-    content = models.TextField() # json
-
-
 class Survey(models.Model):
-    project = models.ForeignKey(Project, related_name='project_surveys')
+    sid = CharField(max_length=20)
+    project = models.ForeignKey(Project, related_namQuee='project_surveys')
+    code = CharField(max_length=4)
+    logo = CharField(max_length=100)
     questions = models.ManyToManyField(QuestionEntry, 
                     through='SurveyMembership', 
                     related_name='questions_surveys')
@@ -62,16 +55,39 @@ class Survey(models.Model):
     name = models.CharField(max_length=50)
     raw_content = models.TextField() # stored raw json of all the survey
     date_created = models.DateTimeField(auto_now=True)
+    others = models.TextField()
+    md5 = models.CharField(max_length=128) # support maximu MD5 128
     
     def __unicode__(self):
         return u'%s @ %s'%(creater, date_created)
 
 
+class QuestionEntry(models.Model):
+    qtype = models.CharField(max_length=20)
+    required = models.BooleanField(default=False)
+    description = models.CharField(max_length=100)
+    options = models.CharField(max_length=200)
+    question = models.CharField(max_length=50)
+    code = models.CharField(max_length=4)
+    content = models.TextField() # json
+    others = models.TextField() # other information
+
+
+class ProjectTesteeMembership(models.Model):
+    project = models.ForeignKey(Project, related_name='project_testee_memberships')
+    testee = models.ForeignKey(User, related_name='testee_project_memberships')
+
+    alias = models.CharField(max_length=50) # testee alias in each project
+    
+
 class SurveyMembership(models.Model):
     qentry = models.ForeignKey(QuestionEntry, related_name='question_memberships')
     survey = models.ForeignKey(Survey, related_name='survey_memberships')
-
+    
     entry_order = models.IntegerField(default=0) # used to store qentry order in each survey
+    
+    class Meta:
+        ordering = ('entry_order',)
 
 
 class Schedule(models.Model):
@@ -80,6 +96,7 @@ class Schedule(models.Model):
     name = models.CharField(max_length=50)
     content = models.TextField(default='') # json
     date_created = models.DateTimeField(auto_now=True)
+    md5 = models.CharField(max_length=128)
 
 
 class Plan(models.Model):
@@ -101,7 +118,8 @@ class Record(models.Model):
     testee = models.ForeignKey(User, related_name='testee_records')
     plan = models.ForeignKey(Plan, related_name='plan_records')
     
-    date_created = models.DateTimeField(auto_now=True)
+    date_created = models.DateTimeField()
+    date_uploaded = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return u'%s @ %s'%(testee, date_created)
