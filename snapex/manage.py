d8de4e6870fbf48128ef099b59f735bf4e90283a
Yuming Tao <Yuming@tu059125.ip.tsinghua.edu.cn>
2014-10-28 15:09:09 +0800

    manage.py modification

--- snapex/manage.py
+++ snapex/manage.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-sys.path.insert(1, '/Users/Yuming/Codes/BAE/appid282gcboc93')
+#sys.path.insert(1, '/Users/Yuming/Codes/BAE/appid282gcboc93')
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "snapex.settings")
