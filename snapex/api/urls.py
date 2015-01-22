a1aee175b3c2fc0c0cd79c66486ed262234c2f65
Yuming Tao <Yuming@Yumings-MacBook-ProR.local>
2015-01-22 13:45:02 +0800

    Debug

--- snapex/api/urls.py
+++ snapex/api/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     url(r'^create_survey$', views.create_survey, name='api_create_survey'),
     url(r'^create_schedule$', views.create_schedule, name='api_create_schedule'),
     url(r'^report$', views.report_record, name='api_report_survey'),
-    url(r'^report_media$', views.report_media_list, name='api_report_media'),
-    url(r'^pull_plans$', views.pull_plans, name='api_pull_plans'),
+    #url(r'^report_media$', views.report_media_list, name='api_report_media'),
+    #url(r'^pull_plans$', views.pull_plans, name='api_pull_plans'),
     url(r'^auth$', views.auth, name='api_auth'),
     url(r'^pull_project$', views.pull_project, name='api_pull_project')
 ]
