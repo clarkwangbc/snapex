ecbd9c327358e7d656ca5d9e4e58d3a79134160b
Yuming Tao <Yuming@Yumings-MacBook-ProR.local>
2015-03-04 11:53:10 +0800

    Error fix

--- snapex/api/urls.py
+++ snapex/api/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     #url(r'^report_media$', views.report_media_list, name='api_report_media'),
     #url(r'^pull_plans$', views.pull_plans, name='api_pull_plans'),
     url(r'^auth$', views.auth, name='api_auth'),
-    url(r'^pull_project$', views.pull_project, name='api_pull_project')
+    url(r'^pull_project$', views.pull_project, name='api_pull_project'),
     url(r'^update_userinfo',views.update_userinfo, name='update_userinfo')
 ]
